61116ac8c56a05b05d170fc9dbdeb97e72e5867e
Ben Hunter <code@benhunter.me>
2023-12-23 01:45:02 -0600

    Add gluestack, improve list of classes

--- App.tsx
+++ App.tsx
@@ -1,32 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { app } from './firebaseConfig';
+import {collection, getDocs, getFirestore} from "firebase/firestore";
+import {useEffect, useState} from "react";
+import {StatusBar} from 'expo-status-bar';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {app} from './firebaseConfig';
 import 'firebase/firestore';
-import { getFirestore } from 'firebase/firestore';
+
+import {GluestackUIProvider, Text, Box, Heading, VStack, ScrollView, Center} from '@gluestack-ui/themed';
+import {config} from '@gluestack-ui/config'; // Optional if you want to use default theme
+
+function AppGlue() {
+  return (
+    <GluestackUIProvider config={config}>
+      <Box width="100%" justifyContent="center" alignItems="center">
+        <Text>Open up App.js to start working on your app!</Text>
+      </Box>
+    </GluestackUIProvider>
+  );
+}
 
 export default function App() {
   let [classes, setClasses] = useState<Class[]>([])
 
   useEffect(() => {
-    // set classes to the result of getClasses()
     getClasses().then((classes) => {
+      classes.sort((a, b) => {
+        return a.ClassName.localeCompare(b.ClassName)
+      });
       setClasses(classes)
     })
   }, [])
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {classes.map((c: Class) => {
-        return <>
-        <Text style={{margin: 10}}>{c.ClassName}</Text>
-        </>
-      })}
-      {classes.map((c: any) => {
-        return <Text>{JSON.stringify(c)}</Text>
-      })}
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider config={config}>
+      <SafeAreaView>
+
+        <Box width="100%" justifyContent="center" alignItems="center">
+          <Heading>Classes</Heading>
+          <ScrollView width="100%" height="100%">
+            <VStack>
+              {classes.map((c: Class) => {
+                return <Box
+                  key={c.id}
+                  maxWidth="$full"
+                  borderColor="$borderLight200"
+                  borderRadius="$lg"
+                  borderWidth="$1"
+                  my="$1"
+                  overflow="hidden"
+                  $base-mx="$5"
+                  $dark-bg="$backgroundDark900"
+                  $dark-borderColor="$borderDark800"
+                >
+                  <Center>
+
+                    <Text m="$2">{c.ClassName}</Text>
+                  </Center>
+                </Box>
+              })}
+              {/*{classes.map((c: any) => {*/}
+              {/*  return <Text>{JSON.stringify(c)}</Text>*/}
+              {/*})}*/}
+            </VStack>
+          </ScrollView>
+          <StatusBar style="auto"/>
+        </Box>
+      </SafeAreaView>
+    </GluestackUIProvider>
   )
 }
 
@@ -39,26 +78,14 @@ const getClasses = async () => {
   const db = getFirestore(app);
   const querySnapshot = await getDocs(collection(db, "Class"));
 
-  querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
-  });
+  // querySnapshot.forEach((doc) => {
+  //   console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+  // });
+
   const classes = querySnapshot.docs.map((doc) => {
     const data = doc.data();
     const id = doc.id;
-    return { ...data, id } as Class;
-  } );
+    return {...data, id} as Class;
+  });
   return classes;
 };
-
-import { collection, getDocs } from "firebase/firestore";
-import {useEffect, useState} from "react";
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
