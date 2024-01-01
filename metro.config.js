c76c7260ca0f264949f43364d15b01ccfd5da3c3
Ben Hunter <code@benhunter.me>
2024-01-01 13:40:54 -0600

    All platforms working in Expo Go! probably needs some cleanup

---
+++ metro.config.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config')
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname)
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts },
+  } = config
+
+  return {
+    ...config,
+    resolver: {
+      ...config.resolver,
+      sourceExts: [...sourceExts, 'mjs', 'cjs'],
+    },
+  }
+})()
+
+// Learn more https://docs.expo.io/guides/customizing-metro
+// const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+// const config = getDefaultConfig(__dirname, {
+  // [Web-only]: Enables CSS support in Metro.
+  // isCSSEnabled: true,
+// });
+
+// module.exports = config;
